47dd5975369bad39e82f1ec5d62555dc8fc02e60
06112497liu <804912547@qq.com>
2017-09-12 00:02:33 +0800

    first commit

--- git/src/HH.java
+++ git/src/HH.java
@@ -1,2 +1,10 @@
 public class HH {
+    public static void main(String[] args) {
+
+        Object ss = "a";
+        Class clazzs = ss.getClass();
+        boolean flag = ((Object)clazzs == (Object)Object[].class);
+        System.out.println(flag);
+    }
+
 }
